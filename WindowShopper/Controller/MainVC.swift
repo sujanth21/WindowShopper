380f0b9764b6a5b9eff42e8a66441943cb4fe3a0
Sujanth <sujanth1987@gmail.com>
2017-08-04 23:38:54 +1000

    Calculate button added programmatically

---
+++ WindowShopper/Controller/MainVC.swift
@@ -0,0 +1,42 @@
+//
+//  ViewController.swift
+//  WindowShopper
+//
+//  Created by Sujanth Sebamalaithasan on 4/8/17.
+//  Copyright © 2017 Sujanth Sebamalaithasan. All rights reserved.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    @IBOutlet weak var wageTxt: CurrencyTxtField!
+    @IBOutlet weak var priceTxt: CurrencyTxtField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        let calcBtn = UIButton(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 60))
+        
+        calcBtn.backgroundColor = #colorLiteral(red: 1, green: 0.7343676843, blue: 0.06855516822, alpha: 1)
+        calcBtn.setTitle("Calculate", for: .normal)
+        calcBtn.setTitleColor(#colorLiteral(red: 1.0, green: 1.0, blue: 1.0, alpha: 1.0), for: .normal)
+        calcBtn.addTarget(self, action: #selector(MainVC.calculate), for: .touchUpInside)
+        
+        wageTxt.inputAccessoryView = calcBtn
+        priceTxt.inputAccessoryView = calcBtn
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @objc func calculate() {
+        print("We got here!")
+    }
+
+
+}
+
